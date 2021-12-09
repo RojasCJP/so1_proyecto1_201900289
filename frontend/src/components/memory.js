450affaea3dc80b6aae1a9d54abf18bab38832bc
juanpa <jprojaschinchilla@gmail.com>
2021-12-08 22:22:25 -0600

    iniciado con las graficas en el frontend

--- frontend/src/components/memory.js
+++ frontend/src/components/memory.js
@@ -1,22 +1,41 @@
 import React from 'react'
+import {Chart} from 'react-google-charts'
 
 export class Memory extends React.Component {
+    state = {
+        data: [['x','Memoria RAM'],[1,2],[2,3],[3,5],[4,7],[5,8],[6,11],[7,1]]
+    }
+    
     render() {
         return (
             <div>
-                <p>
-                    Nombre: Memory
-                </p>
-                <p>
-                    Carnet: 201900289
-                </p>
-                <p>
-                    Laboratorio: Sistemas Operativos 1
-                </p>
-                <p>
-                    Semestre: Vacaciones Diciembre 2021
-                </p>
+                <Chart
+                    width={'800px'}
+                    height={'500px'}
+                    chartType="LineChart"
+                    loader={<div>Loading Chart</div>}
+                    data={this.state.data}
+                    options={{
+                        title: ' ',
+                        backgroundColor:'transparent',
+                        hAxis: {
+                            title: 'Tiempo',
+                        },
+                        vAxis: {
+                            title: 'Uso',
+                        },
+                    }}
+                  
+                    rootProps={{ 'data-testid': '1' }}
+                />
             </div>
         )
+    }
+
+    fillData() {
+        var Data = {
+            labels: ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes"],
+        }
+        this.setState({ data: Data })
     }
 }
--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -5,7 +5,7 @@ export class Navbar extends React.Component {
     render() {
         return (
             <div>
-                <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
                     <div class='container'>
                         <a class="navbar-brand" href='/'>Proyecto 1</a>
                         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
